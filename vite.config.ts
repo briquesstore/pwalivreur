bdcf63155b0a9548a7ff29f6c9592342e3beb994
offoange <andrepierre585@gmail.com>
2026-08-24 15:26:46 +0000

    fix: increase PWA cache limit to 5MB for logo

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       manifest: false,
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,webp}'],
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MB
       },
     }),
   ],
